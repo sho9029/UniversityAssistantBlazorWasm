9f32d051eaecbf8d170721379e1739ef09565e70
sho9029 <yatabe_9029@yahoo.co.jp>
2021-08-11 00:09:43 +0900

    ユーザー機能の追加

--- Models/Auth.cs
+++ Models/Auth.cs
@@ -20,6 +20,18 @@ namespace UniversityAssistantBlazorWasm.Models
     {
         public bool IsSuccessful { get; set; }
         public Exception Error { get; set; }
+        public string ErrorReason
+        {
+            get
+            {
+                if (Error == null)
+                {
+                    return String.Empty;
+                }
+                var message = Error.Message;
+                return message[(message.IndexOf("Reason: ") + 7)..];
+            }
+        }
         public string IDToken { get; set; }
     }
 }
