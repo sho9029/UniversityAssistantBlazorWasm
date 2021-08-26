49b47df71051d35bd585ae843fd6e8f827fb1926
sho9029 <yatabe_9029@yahoo.co.jp>
2021-08-26 19:24:26 +0900

    プッシュ通知送受信機能の実装

---
+++ Models/NotificationPayload.cs
@@ -0,0 +1,16 @@
+﻿namespace UniversityAssistantBlazorWasm.Models
+{
+    public class NotificationPayload
+    {
+        public NotificationPayload(string message, string url, string title = "UniversityAssistant")
+        {
+            Title = title;
+            Message = message;
+            Url = url;
+        }
+
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
+    }
+}
